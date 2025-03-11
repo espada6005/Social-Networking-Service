568805f6107006f9a6e3a4751f03d8e354ad4eff
espada <yua2479@gmail.com>
2025-03-11 10:59:25 +0900

    add: コード生成するCodeGeneration.phpを追加

---
+++ src/Commands/Programs/CodeGeneration.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Commands\Programs;
+
+use Commands\AbstractCommand;
+use Commands\Argument;
+
+class CodeGeneration extends AbstractCommand {
+
+    // 使用するコマンド名を指定
+    protected static ?string $alias = "code-gen";
+    protected static bool $requiredCommandValue = true;
+
+    // 引数を割り当て
+    public static function getArguments(): array {
+        return [
+            (new Argument("name"))->description("Name of the file that is to be generated.")->required(false),
+        ];
+    }
+
+    public function execute(): int {
+        $codeGenType = $this->getCommandValue();
+        $this->log("Generating code for..." . $codeGenType);
+
+        if ($codeGenType == "migration") {
+            $migrationName = $this->getArgumentValue("name");
+            $this->generatedMigrationFile($migrationName); 
+        }
+
+        return 0;
+    }
+
+    private function generatedMigrationFile(string $migrationName): void {
+        $filename = sprintf(
+            "%s_%s_%s.php",
+            date("Y-m-d"),
+            time(),
+            $migrationName
+        );
+
+        $migrationContent = $this->getMigrationContent($migrationName);
+
+        // マイグレーションファイルを保存するディレクトリとパス
+        $directory = sprintf("%s/../../Database/Migrations", __DIR__);
+        $path = sprintf("%s/%s", $directory, $filename);
+
+        // ディレクトリの存在確認と作成
+        if (!is_dir($directory)) {
+            mkdir($directory, 0755, true); // 再帰的にディレクトリを作成
+            $this->log("Directory {$directory} has been created!");
+        }
+
+        file_put_contents($path, $migrationContent);
+        $this->log("Migration file {$filename} has been generated!");
+    }
+
+    private function getMigrationContent(string $migrationName): string {
+        $className = $this->pascalCase($migrationName);
+
+        return <<<MIGRATION
+        <?php
+        namespace Database\Migrations;
+        
+        use Database\SchemaMigration;
+        
+        class {$className} implements SchemaMigration {
+            public function up(): array {
+                // マイグレーションロジックをここに追加してください
+                return [];
+            }
+        
+            public function down(): array {
+                // ロールバックロジックを追加してください
+                return [];
+            }
+        }
+        MIGRATION;
+    }
+
+    private function pascalCase(string $string): string {
+        return str_replace(" ", "", ucwords(str_replace("_", " ", $string)));
+    }
+
+}
