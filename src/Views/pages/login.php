dd1d45fa74d69a188c2967a472f15bb2c0973050
espada <yua2479@gmail.com>
2025-03-22 15:37:44 +0900

    add: ログインページ(login.php)を追加

---
+++ src/Views/pages/login.php
@@ -0,0 +1,9 @@
+
+<div class="login-container">
+    <h1>ログイン</h1>
+    <form id="loginForm">
+        <input type="text" id="username" name="username" placeholder="ユーザー名">
+        <input type="password" id="password" name="password" placeholder="パスワード">
+        <button type="submit">ログイン</button>
+    </form>
+</div>
