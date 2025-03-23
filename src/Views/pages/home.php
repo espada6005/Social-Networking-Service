0d06bf9b674272033f86fa77ee7fae976415494d
espada <yua2479@gmail.com>
2025-03-23 16:39:38 +0900

    add: ホームページ(home.php)を追加

---
+++ src/Views/pages/home.php
@@ -0,0 +1,22 @@
+<header>
+    <h1>Welcome to SNS Service</h1>
+    <p class="tagline">Connect, Share, and Explore</p>
+</header>
+<main>
+    <!-- 注意書きとボタンを囲む枠 -->
+    <div class="container">
+        <div class="buttons">
+            <a href="/login">ログイン</a>
+            <a href="/register">ユーザー登録</a>
+            <div class="separator">
+                <span>または</span>
+            </div>
+            <a href="/login">ゲストログイン</a>
+        </div>
+        <div class="guest-warning">
+            <p>ゲストログインで使用するアカウントは、他のユーザーとの共有アカウントになるため、ログイン後の操作では個人情報や機密情報を扱わないようにしてください。<br>
+                ゲストアカウントの操作による情報漏洩やデータの損失について、当サービスは一切の責任を負いかねます。<br>
+                また、ゲストユーザーのアカウントに紐づくデータは、予告せず管理者側で削除する可能性があります。</p>
+        </div>
+    </div>
+</main>
