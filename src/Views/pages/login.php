2bed3d9a4b73e56f130c4918fc00de1c35e341af
espada <yua2479@gmail.com>
2025-03-23 16:38:15 +0900

    update: コード整形

--- src/Views/pages/login.php
+++ src/Views/pages/login.php
@@ -1,13 +1,11 @@
-<div class="body">
-    <div class="login-container">
-        <h1>ログイン</h1>
-        <form id="loginForm">
-                <label for="username">ユーザー名</label>
-                <input type="text" id="username" name="username">
-                <label for="password">パスワード</label>
-                <input type="password" id="password" name="password">
-            <button type="submit">ログイン</button>
-        </form>
-        <a href="/">ホームへ</a>
-    </div>
+<div class="login-container">
+    <h1>ログイン</h1>
+    <form id="loginForm">
+            <label for="username">ユーザー名</label>
+            <input type="text" id="username" name="username">
+            <label for="password">パスワード</label>
+            <input type="password" id="password" name="password">
+        <button type="submit">ログイン</button>
+    </form>
+    <a href="/">ホームへ</a>
 </div>
