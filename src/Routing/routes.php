bc31a39bd973b7a339f8181333dc3ddcec03e509
espada <yua2479@gmail.com>
2025-04-25 21:34:29 +0900

    update: ""ルート追加

--- src/Routing/routes.php
+++ src/Routing/routes.php
@@ -1,5 +1,12 @@
 <?php
 
+use Response\HTTPRenderer;
+use Response\Render\HTMLRenderer;
+use Routing\Route;
+
 return [
-    
+     // トップページ
+    "" => Route::create("", function (): HTTPRenderer {
+        return new HTMLRenderer("pages/top", []);
+    })->setMiddleware(["guest"]),
 ];
