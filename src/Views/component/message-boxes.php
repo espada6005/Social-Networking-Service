e3a92ef9dc1e8f78b51b4cb68c2a3557baa81c65
espada <yua2479@gmail.com>
2025-04-27 21:06:32 +0900

    update: success時の色と閉じるボタンが表示されるように変更

--- src/Views/component/message-boxes.php
+++ src/Views/component/message-boxes.php
@@ -5,14 +5,16 @@ $error = \Response\FlashData::getFlashData('error');
 
 <div class="container mt-5 mb-5">
     <?php if ($success): ?>
-        <div class="alert alert-success">
+        <div class="alert alert-info alert-dismissible fade show" role="alert">
             <?php echo htmlspecialchars($success); ?>
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>
     <?php endif; ?>
 
     <?php if ($error): ?>
-        <div class="alert alert-danger">
+        <div class="alert alert-danger alert-dismissible fade show" role="alert">
             <?php echo htmlspecialchars($error); ?>
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>
     <?php endif; ?>
 </div>
