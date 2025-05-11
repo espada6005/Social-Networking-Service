1e926deffaddeacfd24725b1bb23ab3bdabb6ba6
espada <yua2479@gmail.com>
2025-05-11 21:36:32 +0900

    fix: 微修正

--- src/Database/DataAccess/Interfaces/UserDAO.php
+++ src/Database/DataAccess/Interfaces/UserDAO.php
@@ -7,14 +7,23 @@ use Models\User;
 interface UserDAO {
 
     public function create(User $user, string $password): bool;
-    public function getById(int $id): ?User;
+
+    public function getById(int $user_id): ?User;
+
     public function getByEmail(string $email): ?User;
+
     public function getByUsername(string $username): ?User;
+
     public function getGuestUser(): ?User;
-    public function getHashedPasswordById(int $id): ?string;
+
+    public function getHashedPasswordById(int $user_id): ?string;
+
     public function update(User $user): bool;
+
     public function updateEmailConfirmedAt(int $id): bool;
-    public function updatePassword(int $id, string $password): bool;
-    public function delete(int $id): bool;
+
+    public function updatePassword(int $user_id, string $password): bool;
+    
+    public function delete(int $user_id): bool;
 
 }
