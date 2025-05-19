dfa07269b6c040dd7fa370483a0f39acce854c44
espada <yua2479@gmail.com>
2025-05-19 22:30:12 +0900

    fix: メッセージの表示位置を調整

--- src/Views/component/message-boxes.php
+++ src/Views/component/message-boxes.php
@@ -1,18 +1,18 @@
 <?php
-$success = \Response\FlashData::getFlashData('success');
-$error = \Response\FlashData::getFlashData('error');
+$success = \Response\FlashData::getFlashData("success");
+$error = \Response\FlashData::getFlashData("error");
 ?>
 
-<div class="container mt-5 mb-5">
+<div class="position-fixed top-0 start-0 end-0 z-3">
     <?php if ($success): ?>
-        <div class="alert alert-info alert-dismissible fade show" role="alert">
+        <div class="alert alert-success alert-dismissible fade show text-center" role="alert">
             <?php echo htmlspecialchars($success); ?>
             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>
     <?php endif; ?>
 
     <?php if ($error): ?>
-        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+        <div class="alert alert-danger alert-dismissible fade show text-center" role="alert">
             <?php echo htmlspecialchars($error); ?>
             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>
