a597fafd188e2599e6a00f7609aa4f8ef059cfad
espada <yua2479@gmail.com>
2025-05-24 14:02:02 +0900

    add: パスワードリセットメール

---
+++ src/Views/mail/password_reset.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>パスワード変更</title>
+</head>
+<body>
+    <p><?php echo htmlspecialchars($toName) . " さま"; ?></p>
+    <p>下のURLをクリックして、パスワードを変更してください。</p>
+    <p><a href="<?php echo htmlspecialchars($signedURL); ?>" style="color: #1a73e8;">パスワードを変更する</a></p>
+    <p>このURLの有効期間は1時間です。</p>
+    <p>このメールに心当たりがない場合は、お手数ですがこのメールを削除してください。</p>
+</body>
+</html>
