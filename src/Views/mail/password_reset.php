b2d2e536ad1bbbd8c8e53adb65f01f6d4255326e
espada <yua2479@gmail.com>
2025-05-25 15:25:22 +0900

    fix: 微修正

--- src/Views/mail/password_reset.php
+++ src/Views/mail/password_reset.php
@@ -7,7 +7,7 @@
 <body>
     <p><?php echo htmlspecialchars($toName) . " さま"; ?></p>
     <p>下のURLをクリックして、パスワードを変更してください。</p>
-    <p><a href="<?php echo htmlspecialchars($signedURL); ?>" style="color: #1a73e8;">パスワードを変更する</a></p>
+    <p><a href="<?php echo htmlspecialchars($signedURL); ?>" style="color: #1a73e8;"><?php echo htmlspecialchars($signedURL); ?></a></p>
     <p>このURLの有効期間は1時間です。</p>
     <p>このメールに心当たりがない場合は、お手数ですがこのメールを削除してください。</p>
 </body>
