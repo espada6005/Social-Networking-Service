f8a744ea6a15ee71f8720dd83bea1f8cec3411d0
espada <yua2479@gmail.com>
2025-06-03 21:06:14 +0900

    add: パスワード変更ページ

---
+++ src/Views/pages/password_reset.php
@@ -0,0 +1,42 @@
+<div class="container-fluid bg-light pt-5" style="min-height: 100vh;">
+    <div class="row">
+        <div class="col-md-6 offset-md-3">
+            <div class="card py-5 px-3 my-3">
+                <h2 class="text-center mb-4">パスワードリセット</h2>
+                <form id="password-reset-form" method="post">
+                    <input type="hidden" name="csrf_token" value="<?= Helpers\CrossSiteForgeryProtection::getToken(); ?>">
+                    <input type="hidden" name="user_id" value="<?= $userId ?>">
+                    <div class="mb-3">
+                        <label for="password" class="form-label">新しいパスワード</label>
+                        <input type="password" class="form-control" id="password" name="password" required>
+                        <div id="password-error-msg" class="invalid-feedback"></div>
+                        <div class="form-text">
+                            <p class="m-0">以下の条件を満たすパスワードを設定してください。</p>
+                            <p class="m-0 ms-2">- 8文字以上30文字以下</p>
+                            <p class="m-0 ms-2">- 1文字以上の大文字</p>
+                            <p class="m-0 ms-2">- 1文字以上の小文字</p>
+                            <p class="m-0 ms-2">- 1文字以上の数値</p>
+                            <p class="m-0 ms-2">- 1文字以上の特殊文字（アルファベット以外の文字）</p>
+                        </div>
+                    </div>
+                    <div class="mb-3">
+                        <label for="confirm-password" class="form-label">パスワード（確認）</label>
+                        <input type="password" class="form-control" id="confirm-password" name="confirm-password" required>
+                        <div id="confirm-password-error-msg" class="invalid-feedback"></div>
+                    </div>
+                    <div class="text-center mt-5">
+                        <button id="password-reset-btn" type="submit" class="btn btn-primary w-100">
+                            パスワードリセット
+                            <div id="btn-spinner" class="spinner-border spinner-border-sm text-light d-none" role="status">
+                                <span class="visually-hidden">Loading...</span>
+                            </div>
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script src="/js/common.js"></script>
+<script src="/js/password_reset.js"></script>
