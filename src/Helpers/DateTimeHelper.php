ec78fb3729f541aaac25758fd2cd95cd2be73177
espada <yua2479@gmail.com>
2025-06-17 16:20:55 +0900

    update: getTimeDiffメソッドを追加

--- src/Helpers/DateTimeHelper.php
+++ src/Helpers/DateTimeHelper.php
@@ -17,6 +17,38 @@ class DateTimeHelper {
         return self::$timezone;
     }
 
+    public static function getTimeDiff(string $dateTimeString): string {
+        // 現在時刻データ
+        $now = new DateTime("now", self::getTimezone());
+
+        // 比較対象の日時データ
+        $dateTime = new DateTime($dateTimeString, self::getTimezone());
+
+        // 経過時間を秒単位で取得
+        $seconds = $now->getTimestamp() - $dateTime->getTimestamp();
+
+        if ($seconds < 60) {
+            // 1分未満
+            return $seconds . "秒前";
+        } elseif ($seconds < 3600) {
+            // 1時間未満
+            $minutes = floor($seconds / 60);
+            return $minutes . "分前";
+        } elseif ($seconds < 86400) {
+            // 1日未満
+            $hours = floor($seconds / 3600);
+            return $hours . "時間前";
+        } elseif ($seconds < 86400 * 7) {
+            // 1週間未満
+            $days = floor($seconds / 86400);
+            return $days . "日前";
+        } else {
+            // 1週間以上の場合は日付を表示
+            $format = $dateTime->format("Y") === $now->format("Y") ? "n月j日" : "Y年n月j日";
+            return $dateTime->format($format);
+        }
+    }
+
     public static function getCurrentDateTime(): DateTime {
         return new DateTime("now", self::getTimeZone());
     }
