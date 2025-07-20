9ef0c742c36afcb7d6691c92aca34ba2bc3860d8
espada <yua2479@gmail.com>
2025-07-20 22:12:18 +0900

    update: エラーメッセージの表示

--- public/js/profile.js
+++ public/js/profile.js
@@ -19,8 +19,8 @@ document.addEventListener("DOMContentLoaded", async function () {
     function initprofile(user) {
         const nameEl = document.querySelector("#profile-name");
         const usernameEl = document.querySelector("#profile-username");
-        const profileTextEl = document.querySelector("#profile-text");
-        const profileImageEl = document.querySelector("#profile-image");
+        const profileTextEl = document.querySelector("#profile-text-area");
+        const profileImageEl = document.querySelector("#profile-image-area");
         const profileImageLinkEl = document.querySelector("#profile-image-link");
         const followeeCountEl = document.querySelector("#followee-count");
         const followerCountEl = document.querySelector("#follower-count");
@@ -138,6 +138,7 @@ document.addEventListener("DOMContentLoaded", async function () {
     const form = document.querySelector("#profile-edit-form");
     form.addEventListener("submit", async function (event) {
         event.preventDefault();
+        resetFormValidations();
 
         const formData = new FormData(form);
         const responseJson = await sendPostRequest("form/profile/update", formData);
@@ -149,185 +150,186 @@ document.addEventListener("DOMContentLoaded", async function () {
         if (responseJson.status === "success") {
             window.location.reload();
             console.log("プロフィール更新成功");
-        } else {
-            if (responseJson.status === "fieldErrors") {
-                for (const field in responseJson.fieldErrors) {
-                    setFormValidation(field, responseJson.fieldErrors[field]);
-                }
+        }
+
+        if (responseJson.status === "fieldErrors") {
+            for (const field in responseJson.message) {
+                setFormValidation(field, responseJson.message[field]);
             }
-            if (responseJson.error) {
-                alert(responseJson.error);
-            }
+        }
+
+        if (responseJson.error) {
+            alert(responseJson.error);
         }
     });
 
-    /**
-   * フォローボタンクリック時の処理
-   */
-    const followBtn = document.querySelector("#profile-follow-btn");
-    followBtn.addEventListener("click", async function (event) {
-        event.preventDefault();
+/**
+* フォローボタンクリック時の処理
+*/
+const followBtn = document.querySelector("#profile-follow-btn");
+followBtn.addEventListener("click", async function (event) {
+    event.preventDefault();
 
-        const csrfToken = document.querySelector("#csrf-token").dataset.token;
-        const formData = new FormData();
-        formData.append("user", user ?? "");
-        formData.append("csrf_token", csrfToken);
-        const responseJson = await sendPostRequest("follow", formData);
+    const csrfToken = document.querySelector("#csrf-token").dataset.token;
+    const formData = new FormData();
+    formData.append("user", user ?? "");
+    formData.append("csrf_token", csrfToken);
+    const responseJson = await sendPostRequest("follow", formData);
 
-        if (responseJson === null) {
-            alert("エラーが発生しました。");
-        }
-
-        if (responseJson.status === "success") {
-            window.location.reload();
-        } else {
-            if (responseJson.status === "error") {
-                alert(responseJson.message);
-            }
-        }
-    });
-
-    /**
-     * アンフォローボタンクリック時の処理
-     */
-    const unfollowBtn = document.querySelector("#profile-unfollow-btn");
-    unfollowBtn.addEventListener("click", async function (event) {
-        event.preventDefault();
-        
-        const csrfToken = document.querySelector("#csrf-token").dataset.token;
-        const formData = new FormData();
-        formData.append("user", user ?? "");
-        formData.append("csrf_token", csrfToken);
-        const responseJson = await sendPostRequest("unfollow", formData);
-
-        if (responseJson === null) {
-            alert("エラーが発生しました。");
-        }
-
-        if (responseJson.status === "success") {
-            window.location.reload();
-        } else {
-            if (responseJson.status === "error") {
-                alert(responseJson.message);
-            }
-        }
-    });
-
-    /**
-    * ポスト一覧初期化処理
-    */
-    const postBlock = document.querySelector("#post-block");
-    const spinner = document.querySelector("#spinner");
-    const limit = 30;
-    const listData = {
-        posts: {
-            offset: 0,
-            loadAll: false,
-            listEl: document.querySelector("#posts-list"),
-        },
-        replies: {
-            offset: 0,
-            loadAll: false,
-            listEl: document.querySelector("#replies-list")
-        },
-        likes: {
-            offset: 0,
-            loadAll: false,
-            listEl: document.querySelector("#likes-list"),
-        },
+    if (responseJson === null) {
+        alert("エラーが発生しました。");
     }
 
-    async function loadList(listType = "posts") {
-        const csrfToken = document.querySelector("#csrf-token").dataset.token;
-        const params = new URLSearchParams(window.location.search);
-        const user = params.get("user");
-        const formData = new FormData();
-        formData.append("user", user ?? "");
-        formData.append("limit", limit);
-        formData.append("offset", listData[listType].offset ?? 0);
-        formData.append("csrf_token", csrfToken);
-        const responseJson = await sendPostRequest(`/${listType}/init`, formData);
-
-        if (responseJson.status === "success") {
-            if (responseJson.posts.length) {
-                for (const post of responseJson.posts) {
-                    createPostEl(post, listData[listType].listEl);
-                }
-                listData[listType].offset += limit;
-            } else {
-                listData[listType].loadAll = true;
-
-                if (listData[listType].offset === 0) {
-                    document.querySelector("#post-not-found").classList.remove("d-none");
-                    document.querySelector("#list-wrapper").classList.add("d-none");
-                }
-            }
-            spinner.classList.add("d-none");
-            postBlock.classList.remove("d-none");
-            postBlock.classList.add("d-flex", "flex-column");
-        } else {
-            if (responseJson.status === "error") {
-                alert(responseJson.message);
-            }
+    if (responseJson.status === "success") {
+        window.location.reload();
+    } else {
+        if (responseJson.status === "error") {
+            alert(responseJson.message);
         }
     }
-    await loadList();
+});
 
-    /**
-     * ポスト種類タブ切り替え時の処理
-     */
-    let activeTab = "posts";
-    document.querySelectorAll("#post-type-tabs .nav-link").forEach(link => {
-        link.addEventListener("click", async function (event) {
-            event.preventDefault();
+/**
+ * アンフォローボタンクリック時の処理
+ */
+const unfollowBtn = document.querySelector("#profile-unfollow-btn");
+unfollowBtn.addEventListener("click", async function (event) {
+    event.preventDefault();
 
-            // 全てのnav-linkからactiveクラスを削除
-            document.querySelectorAll("#post-type-tabs .nav-link").forEach(item => {
-                item.classList.remove("active");
-            });
+    const csrfToken = document.querySelector("#csrf-token").dataset.token;
+    const formData = new FormData();
+    formData.append("user", user ?? "");
+    formData.append("csrf_token", csrfToken);
+    const responseJson = await sendPostRequest("unfollow", formData);
 
-            // クリックされたnav-linkにactiveクラスを追加
-            this.classList.add("active");
+    if (responseJson === null) {
+        alert("エラーが発生しました。");
+    }
 
-            // 全てのセクションを非表示
-            document.querySelectorAll("div[id$='-list']").forEach(section => {
-                section.classList.add("d-none");
-            });
+    if (responseJson.status === "success") {
+        window.location.reload();
+    } else {
+        if (responseJson.status === "error") {
+            alert(responseJson.message);
+        }
+    }
+});
 
-            // クリックされたリンクに対応するセクションを表示
-            const target = document.querySelector(this.getAttribute("data-target"));
-            if (target) {
-                target.classList.remove("d-none");
+/**
+* ポスト一覧初期化処理
+*/
+const postBlock = document.querySelector("#post-block");
+const spinner = document.querySelector("#spinner");
+const limit = 30;
+const listData = {
+    posts: {
+        offset: 0,
+        loadAll: false,
+        listEl: document.querySelector("#posts-list"),
+    },
+    replies: {
+        offset: 0,
+        loadAll: false,
+        listEl: document.querySelector("#replies-list")
+    },
+    likes: {
+        offset: 0,
+        loadAll: false,
+        listEl: document.querySelector("#likes-list"),
+    },
+}
+
+async function loadList(listType = "posts") {
+    const csrfToken = document.querySelector("#csrf-token").dataset.token;
+    const params = new URLSearchParams(window.location.search);
+    const user = params.get("user");
+    const formData = new FormData();
+    formData.append("user", user ?? "");
+    formData.append("limit", limit);
+    formData.append("offset", listData[listType].offset ?? 0);
+    formData.append("csrf_token", csrfToken);
+    const responseJson = await sendPostRequest(`/${listType}/init`, formData);
+
+    if (responseJson.status === "success") {
+        if (responseJson.posts.length) {
+            for (const post of responseJson.posts) {
+                createPostEl(post, listData[listType].listEl);
             }
+            listData[listType].offset += limit;
+        } else {
+            listData[listType].loadAll = true;
 
-            // 表示を戻す
-            document.querySelector("#post-not-found").classList.add("d-none");
-            document.querySelector("#list-wrapper").classList.remove("d-none");
-
-            // リストのスクロールをトップに戻す
-            const listWrapper = document.querySelector("#list-wrapper");
-            listWrapper.scrollTop = 0;
-            activeTab = this.id.replace("-nav-link", "");
-            if (listData[activeTab].offset === 0) {
-                await loadList(activeTab);
+            if (listData[listType].offset === 0) {
+                document.querySelector("#post-not-found").classList.remove("d-none");
+                document.querySelector("#list-wrapper").classList.add("d-none");
             }
+        }
+        spinner.classList.add("d-none");
+        postBlock.classList.remove("d-none");
+        postBlock.classList.add("d-flex", "flex-column");
+    } else {
+        if (responseJson.status === "error") {
+            alert(responseJson.message);
+        }
+    }
+}
+await loadList();
+
+/**
+ * ポスト種類タブ切り替え時の処理
+ */
+let activeTab = "posts";
+document.querySelectorAll("#post-type-tabs .nav-link").forEach(link => {
+    link.addEventListener("click", async function (event) {
+        event.preventDefault();
+
+        // 全てのnav-linkからactiveクラスを削除
+        document.querySelectorAll("#post-type-tabs .nav-link").forEach(item => {
+            item.classList.remove("active");
         });
-    });
 
+        // クリックされたnav-linkにactiveクラスを追加
+        this.classList.add("active");
 
-    /**
-     * list-wrapperのスクロール時の処理
-     */
-    document.querySelector("#list-wrapper").addEventListener("scroll", async function () {
-        const content = this;
+        // 全てのセクションを非表示
+        document.querySelectorAll("div[id$='-list']").forEach(section => {
+            section.classList.add("d-none");
+        });
 
-        // 要素がスクロールの最下部に達したかを確認
-        if (content.scrollTop + content.clientHeight >= content.scrollHeight) {
-            if (!listData[activeTab].loadAll) {
-                spinner.classList.remove("d-none");
-                await loadList(activeTab);
-            }
+        // クリックされたリンクに対応するセクションを表示
+        const target = document.querySelector(this.getAttribute("data-target"));
+        if (target) {
+            target.classList.remove("d-none");
+        }
+
+        // 表示を戻す
+        document.querySelector("#post-not-found").classList.add("d-none");
+        document.querySelector("#list-wrapper").classList.remove("d-none");
+
+        // リストのスクロールをトップに戻す
+        const listWrapper = document.querySelector("#list-wrapper");
+        listWrapper.scrollTop = 0;
+        activeTab = this.id.replace("-nav-link", "");
+        if (listData[activeTab].offset === 0) {
+            await loadList(activeTab);
         }
     });
+});
+
+
+/**
+ * list-wrapperのスクロール時の処理
+ */
+document.querySelector("#list-wrapper").addEventListener("scroll", async function () {
+    const content = this;
+
+    // 要素がスクロールの最下部に達したかを確認
+    if (content.scrollTop + content.clientHeight >= content.scrollHeight) {
+        if (!listData[activeTab].loadAll) {
+            spinner.classList.remove("d-none");
+            await loadList(activeTab);
+        }
+    }
+});
 
 });
